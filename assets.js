e9f060807b4b54b20aafe5ddaf51541b24d8d781
nehal-05 <nehal.goel@torontomu.ca>
2024-12-19 15:11:47 -0500

    Add files via upload

---
+++ assets.js
@@ -0,0 +1,190 @@
+import add_icon_green from './icons8-add-50.png'
+import food_1 from './food_1.jpg'
+import food_2 from './food_2.png'
+import food_3 from './food_3.jpg'
+import food_4 from './food_4.jpg'
+import food_5 from './food_5.jpg'
+import food_6 from './food_6.jpg'
+import food_7 from './food_7.jpg'
+import food_8 from './food_8.png'
+import food_9 from './food_9.webp'
+import food_10 from './food_10.jpg'
+import food_11 from './food_11.jpg'
+import food_12 from './food_12.jpg'
+import logo from './logo.png'
+import add_icon_white from './add-white.png'
+import remove_icon from './remove_icon.png'
+import search_icon from './icon-search.png'
+import basket_icon from './icon-basket.png'
+import facebook_icon from './facebook.png'
+import twitter_icon from './twitter.png'
+import linkedin_icon from './linkedin.png'
+import twitter_icon from './twitter.png'
+import cross_icon from './close.png'
+import play_store from './playstore.png'
+import app_store from './app-store.png'
+import header_img from './header_img.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import rating_stars from './rating_stars.png'
+
+
+export const assets = {
+    add_icon_green,
+    add_icon_white,
+    remove_icon,
+    cross_icon,
+    food_1,
+    food_2,
+    food_3,
+    food_4,
+    food_5,
+    food_6,
+    food_7,
+    food_8,
+    food_9,
+    food_10,
+    food_11,
+    food_12,
+    facebook_icon,
+    linkedin_icon,
+    twitter_icon,
+    play_store,
+    app_store,
+    logo,
+    search_icon,
+    basket_icon,
+    header_img,
+    rating_stars
+}
+
+export const menu_list = [
+    {
+        menu_name: "Salad",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Pasta",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Desserts",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Noodles",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Rolls",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Sandwich",
+        menu_image: menu_6
+    }
+]
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "Greek salad",
+        image: food_1,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Salad"
+    },
+    {
+        _id: "2",
+        name: "Chicken Salad",
+        image: food_2,
+        price: 14,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Salad"
+    },
+    {
+        _id: "3",
+        name: "Chicken Roll",
+        image: food_3,
+        price: 14,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Rolls"
+    },
+    {
+        _id: "4",
+        name: "Veggie Roll",
+        image: food_4,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Rolls"
+    },
+    {
+        _id: "4",
+        name: "Alfredo Pasta",
+        image: food_5,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Pasta"
+    },
+    {
+        _id: "6",
+        name: "Vodka Pasta",
+        image: food_6,
+        price: 16,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Pasta"
+    },
+    {
+        _id: "7",
+        name: "Milk Cake",
+        image: food_7,
+        price: 10,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Desserts"
+    },
+    {
+        _id: "8",
+        name: "Skillet Cookie",
+        image: food_8,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Desserts"
+    },
+    {
+        _id: "9",
+        name: "Ramen",
+        image: food_9,
+        price: 16,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Noodles"
+    },
+    {
+        _id: "10",
+        name: "Lo Mein",
+        image: food_10,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Noodles"
+    },
+    {
+        _id: "11",
+        name: "Veggie Sandwich",
+        image: food_11,
+        price: 13,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Sandwich"
+    },
+    {
+        _id: "12",
+        name: "Tukey Sandwich",
+        image: food_12,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Sandwich"
+    }
+
+]
